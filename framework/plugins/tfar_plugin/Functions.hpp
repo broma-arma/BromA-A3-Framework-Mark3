b82a36184b1a35f26e290f72aa5af86b83e71fbb
Coryf88 <Coryf88@users.noreply.github.com>
2024-07-31 16:22:14 -0500

    Remove deprecated TFAR plugin function, BRM_TFAR_fnc_aiHearsTFAR

--- framework/plugins/tfar_plugin/Functions.hpp
+++ framework/plugins/tfar_plugin/Functions.hpp
@@ -3,8 +3,4 @@ class BRM_TFAR {
 		file = "framework\plugins\tfar_plugin\functions";
 		class postInit { postInit=1; };
 	};
-	class Functions {
-		file = "framework\plugins\tfar_plugin\functions";
-		class AIHearsTFAR { postInit=1; };
-	};
 };

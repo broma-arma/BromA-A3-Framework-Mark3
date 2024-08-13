69c0a2d92cc51659b5f3c438a7e65d21bf85bc90
Coryf88 <Coryf88@users.noreply.github.com>
2024-08-13 14:39:55 -0500

    Add more options for Civilian death limit, Time limit, and Time added per objective

--- framework/plugins/civilian_casualty_cap/Parameters.hpp
+++ framework/plugins/civilian_casualty_cap/Parameters.hpp
@@ -1,6 +1,6 @@
 class p_dead_civies {
 	title = "Civilian death limit";
-	values[] = {       -1,   5,   15,   30};
-	texts[] = {"Disabled", "5", "15", "30"};
+	values[] = {       -1,   1,   2,   3,   4,   5,   15,   30,   50,   100};
+	texts[] = {"Disabled", "1", "2", "3", "4", "5", "15", "30", "50", "100"};
 	default = -1;
 };
--- framework/plugins/time_limit/Parameters.hpp
+++ framework/plugins/time_limit/Parameters.hpp
@@ -1,13 +1,13 @@
 class p_time_limit {
 	title = "Time limit";
-	values[] = {       -1,         60,          600,          900,         1800,         2700,     3600,      7200,     10800};
-	texts[] = {"No limit", "1 minute", "10 minutes", "15 minutes", "30 minutes", "45 minutes", "1 hour", "2 hours", "3 hours"};
+	values[] = {       -1,         60,         300,          600,          900,         1800,         2700,     3600,      7200,     10800};
+	texts[] = {"No limit", "1 minute", "5 minutes", "10 minutes", "15 minutes", "30 minutes", "45 minutes", "1 hour", "2 hours", "3 hours"};
 	default = -1;
 };
 
 class p_time_added {
 	title = "Time added per objective";
-	values[] = {        0,           5,           15,           30,       60};
-	texts[] = {"Disabled", "5 minutes", "15 minutes", "30 minutes", "1 hour"};
+	values[] = {        0,          1,           5,           10,           15,           30,           45,       60};
+	texts[] = {"Disabled", "1 minute", "5 minutes", "10 minutes", "15 minutes", "30 minutes", "45 minutes", "1 hour"};
 	default = 0;
 };

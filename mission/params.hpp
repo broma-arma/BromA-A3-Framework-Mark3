7f958f5ad128dec5c85d73cf284abd6b24a40f2e
Coryf88 <Coryf88@users.noreply.github.com>
2024-08-17 16:37:26 -0500

    Add example mission parameter file

---
+++ mission/params.hpp
@@ -0,0 +1,15 @@
+// Create any Mission Parameters for your mission.
+
+/*
+class bar_mission { title = "MISSION SETTINGS"; values[] = {0}; texts[] = {""}; default = 0; };
+
+// Example mission parameter, see https://community.bistudio.com/wiki/Mission_Parameters#Secondary_Params for more details.
+class misssion_exampleMissionParameter {
+	title = "Example Mission Parameter";
+	values[] = {      -1,   1,   2,   95};
+	texts[] = {"Disable", "1", "2", "95"};
+	default = -1;
+};
+
+class bar_mission_sep { title = ""; values[] = {0}; texts[] = {""}; default = 0; };
+*/

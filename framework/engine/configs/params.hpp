e66902608fa252e89fa8815bacfb70b161125f63
Coryf88 <Coryf88@users.noreply.github.com>
2024-08-17 16:20:45 -0500

    Move plugins.hpp to engine\configs

--- framework/engine/configs/params.hpp
+++ framework/engine/configs/params.hpp
@@ -1,23 +1,23 @@
 class bar_ai { title = "AI SETTINGS"; values[] = {0}; texts[] = {""}; default = 0; };
 #define BRM_PARAMS_AI
-	#include "..\..\..\framework\plugins\plugins.hpp"
+	#include "plugins.hpp"
 #undef BRM_PARAMS_AI
 class bar_ai_sep { title = ""; values[] = {0}; texts[] = {""}; default = 0; };
 
 class bar_conditions { title = "MISSION CONDITIONS"; values[] = {0}; texts[] = {""}; default = 0; };
 #define BRM_PARAMS_CONDITIONS
-	#include "..\..\..\framework\plugins\plugins.hpp"
+	#include "plugins.hpp"
 #undef BRM_PARAMS_CONDITIONS
 class bar_conditions_sep { title = ""; values[] = {0}; texts[] = {""}; default = 0; };
 
 class bar_time { title = "TIME AND MISSION CONSTRAINTS"; values[] = {0}; texts[] = {""}; default = 0; };
 #define BRM_PARAMS_TIME
-	#include "..\..\..\framework\plugins\plugins.hpp"
+	#include "plugins.hpp"
 #undef BRM_PARAMS_TIME
 class bar_time_sep { title = ""; values[] = {0}; texts[] = {""}; default = 0; };
 
 class bar_player { title = "PLAYER SETTINGS"; values[] = {0}; texts[] = {""}; default = 0; };
 #define BRM_PARAMS_PLAYER
-	#include "..\..\..\framework\plugins\plugins.hpp"
+	#include "plugins.hpp"
 #undef BRM_PARAMS_PLAYER
 class bar_player_sep { title = ""; values[] = {0}; texts[] = {""}; default = 0; };
--- framework/engine/configs/plugins.hpp
+++ framework/engine/configs/plugins.hpp
@@ -1,139 +1,139 @@
-#include "..\..\mission\plugins.hpp"
+#include "..\..\..\mission\plugins.hpp"
 
 #ifdef HEADLESS_CLIENT
-#include "headless_client\main.hpp"
+#include "..\..\plugins\headless_client\main.hpp"
 #endif
 
 #ifdef MISSION_CONDITIONS
-#include "mission_conditions\main.hpp"
+#include "..\..\plugins\mission_conditions\main.hpp"
 #endif
 
 #ifdef ACE3_PLUGIN
-#include "ace3_plugin\main.hpp"
+#include "..\..\plugins\ace3_plugin\main.hpp"
 #endif
 
 #ifdef RESPAWN_SYSTEM
-#include "respawn_system\main.hpp"
+#include "..\..\plugins\respawn_system\main.hpp"
 #endif
 
 #ifdef DAC_PLUGIN
-#include "dac_plugin\main.hpp"
+#include "..\..\plugins\dac_plugin\main.hpp"
 #endif
 
 #ifdef TIME_ACCEL
-#include "time_accel\main.hpp"
+#include "..\..\plugins\time_accel\main.hpp"
 #endif
 
 #ifdef VANILLA_SPECTATOR
-#include "vanilla_spectator\main.hpp"
+#include "..\..\plugins\vanilla_spectator\main.hpp"
 #endif
 
 #ifdef TFAR_PLUGIN
-#include "tfar_plugin\main.hpp"
+#include "..\..\plugins\tfar_plugin\main.hpp"
 #endif
 
 #ifdef ACRE2_PLUGIN
-#include "acre2_plugin\main.hpp"
+#include "..\..\plugins\acre2_plugin\main.hpp"
 #endif
 
 #ifdef SPAWN_AI
-#include "spawn_ai\main.hpp"
+#include "..\..\plugins\spawn_ai\main.hpp"
 #endif
 
 #ifdef BLOCK_TP
-#include "block_tp\main.hpp"
+#include "..\..\plugins\block_tp\main.hpp"
 #endif
 
 #ifdef F_EVADE_ESCAPE
-#include "f_evade_escape\main.hpp"
+#include "..\..\plugins\f_evade_escape\main.hpp"
 #endif
 
 #ifdef F_REMOVE_BODY
-#include "f_remove_body\main.hpp"
+#include "..\..\plugins\f_remove_body\main.hpp"
 #endif
 
 #ifdef SPAWN_PROTECTION
-#include "spawn_protection\main.hpp"
+#include "..\..\plugins\spawn_protection\main.hpp"
 #endif
 
 #ifdef MAP_COVER
-#include "map_cover\main.hpp"
+#include "..\..\plugins\map_cover\main.hpp"
 #endif
 
 #ifdef SETUP_ZONE
-#include "setup_zone\main.hpp"
+#include "..\..\plugins\setup_zone\main.hpp"
 #endif
 
 #ifdef CH_VIEW_DISTANCE
-#include "ch_view_distance\main.hpp"
+#include "..\..\plugins\ch_view_distance\main.hpp"
 #endif
 
 #ifdef CIVILIAN_CASUALTY_CAP
-#include "civilian_casualty_cap\main.hpp"
+#include "..\..\plugins\civilian_casualty_cap\main.hpp"
 #endif
 
 #ifdef TIME_LIMIT
-#include "time_limit\main.hpp"
+#include "..\..\plugins\time_limit\main.hpp"
 #endif
 
 #ifdef F_CASUALTIES_CAP
-#include "f_casualties_cap\main.hpp"
+#include "..\..\plugins\f_casualties_cap\main.hpp"
 #endif
 
 #ifdef PREVENT_RESLOT
-#include "prevent_reslot\main.hpp"
+#include "..\..\plugins\prevent_reslot\main.hpp"
 #endif
 
 #ifdef SYNC_STATUS
-#include "sync_status\main.hpp"
+#include "..\..\plugins\sync_status\main.hpp"
 #endif
 
 #ifdef FRIENDLY_FIRE
-#include "friendly_fire\main.hpp"
+#include "..\..\plugins\friendly_fire\main.hpp"
 #endif
 
 #ifdef AO_LIMIT
-#include "ao_limit\main.hpp"
+#include "..\..\plugins\ao_limit\main.hpp"
 #endif
 
 #ifdef TEAM_ROSTER
-#include "team_roster\main.hpp"
+#include "..\..\plugins\team_roster\main.hpp"
 #endif
 
 #ifdef JIP_TELEPORT
-#include "jip_teleport\main.hpp"
+#include "..\..\plugins\jip_teleport\main.hpp"
 #endif
 
 #ifdef COMMANDER_LOCK
-#include "commander_lock\main.hpp"
+#include "..\..\plugins\commander_lock\main.hpp"
 #endif
 
 #ifdef RADIOFREQ
-#include "radiofreq\main.hpp"
+#include "..\..\plugins\radiofreq\main.hpp"
 #endif
 
 #ifdef MISSION_MAKER_TOOLS
-#include "mission_maker_tools\main.hpp"
+#include "..\..\plugins\mission_maker_tools\main.hpp"
 #endif
 
 #ifdef LOADING_SCREEN
-#include "loading_screen\main.hpp"
+#include "..\..\plugins\loading_screen\main.hpp"
 #endif
 
 #ifdef INTROS
-#include "intros\main.hpp"
+#include "..\..\plugins\intros\main.hpp"
 #endif
 
 #ifdef PLANK_BUILDING
-#include "plank_building\main.hpp"
+#include "..\..\plugins\plank_building\main.hpp"
 #endif
 
 #ifdef ROUND_SYSTEM
-#include "round_system\main.hpp"
+#include "..\..\plugins\round_system\main.hpp"
 #endif
 
 #ifdef ACE3_SPECTATOR
-#include "ace3_spectator\main.hpp"
+#include "..\..\plugins\ace3_spectator\main.hpp"
 #endif
 
 #undef HEADLESS_CLIENT

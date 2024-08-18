59a7db4456155a6193711178df8600ef84f72d33
Coryf88 <Coryf88@users.noreply.github.com>
2024-08-18 16:09:01 -0500

    Fix indentation in plugins hpp

--- framework/plugins/f_casualties_cap.hpp
+++ framework/plugins/f_casualties_cap.hpp
@@ -6,11 +6,10 @@
 #endif
 
 #ifdef BRM_PARAMS_TIME
-class p_cas_cap {
-	title = "Casualty Cap";
-	values[] = {       -1,    80,    90,    95,    100};
-	texts[] = {"Disabled", "80%", "90%", "95%", "100%"};
-	default = -1;
-};
-
+	class p_cas_cap {
+		title = "Casualty Cap";
+		values[] = {       -1,    80,    90,    95,    100};
+		texts[] = {"Disabled", "80%", "90%", "95%", "100%"};
+		default = -1;
+	};
 #endif
--- framework/plugins/headless_client.hpp
+++ framework/plugins/headless_client.hpp
@@ -6,10 +6,10 @@
 #endif
 
 #ifdef BRM_PARAMS_AI
-class p_enable_hc {
-	title = "Headless Client";
-	values[] = {        0,         1,      2};
-	texts[] = {"Disabled", "Enabled", "Auto"};
-	default = 2;
-};
+	class p_enable_hc {
+		title = "Headless Client";
+		values[] = {        0,         1,      2};
+		texts[] = {"Disabled", "Enabled", "Auto"};
+		default = 2;
+	};
 #endif
